7df3339c72746d2bc70c8ffd74caf5f67ad002ce
Franco <francodeanquin@gmail.com>
2023-11-29 15:33:39 -0300

    Se realizo la landing page

--- front/src/components/home/home-comp/Footer.jsx
+++ front/src/components/home/home-comp/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import "./Footer.component.css"
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <h2>
+        FOOTER
+      </h2>
+    </div>
   )
 }
---
+++ front/src/components/home/home-comp/Registrate.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Imagen from "../../../assets/insert-image.png"
+import "./Registrate.component.css"
+
+const Registrate = () => {
+  return (
+    <div className='registrate'>
+   <h2>REGISTRATE AQUI</h2>
+
+   <div className='registrate-right'>
+    <h2>iPunto</h2>
+  <a href=""><img src={Imagen} alt="" /></a>
+    </div>
+    </div>
+  )
+}
+
+export default Registrate
