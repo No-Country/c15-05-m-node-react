759f5c19c0a088636437b90a867b48945151a166
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-12-05 17:44:36 -0300

    cambio tamaño tipografia y ancho de card para respetar figma

--- front/src/components/Catalogo/card/Card.jsx
+++ front/src/components/Catalogo/card/Card.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Card = (props) => {
     
     return (
-    <div className="w-80 h-auto m-4 bg-white rounded-xl flex flex-col justify-center items-center">
+    <div className="w-64 h-auto m-4 bg-white rounded-xl flex flex-col justify-center items-center">
         <img className="w-4/5 h-auto pt-6 pb-2" src={props.image.url} alt={`imagen de ${props.name}`}/> 
         <div className="w-full text-left ml-14">
-            <h1 className="text-xl font-semibold text-left" >{props.name}</h1>
+            <h1 className="text-lg font-semibold" >{props.name}</h1>
         </div>
-        <div className="w-52 h-auto m-4 mt-0 ">
-            <div className="flex flex-row justify-evenly">
+        <div className="w-44 h-auto m-4 mt-0 ">
+            <div className="flex flex-row justify-evenly text-sm">
                 {props.category.length && props.category.map((c)=> <h3>{c}</h3>)}
             </div>
             <div className="flex flex-row justify-between">
