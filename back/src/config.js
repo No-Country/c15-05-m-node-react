72726d5202fe0f5a1672f5f7fa4a734c93846d1b
TorresRichardtorrot <torresdrichar@gmail.com>
2023-12-06 18:32:47 -0400

    recuperar password

--- back/src/config.js
+++ back/src/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const port = process.env.PORT;
 export const urlFrond = process.env.URL_FRONT_END
 export const mongodb_uri_key = process.env.MONGO_URI_KEY;
-
+export const urlBackend = process.env.URL_BACKEND
 //  ? JWT
 export const token_secret = process.env.TOKEN_SECRET;
 
--- back/src/routes/recoverPassword.routes.js
+++ back/src/routes/recoverPassword.routes.js
@@ -1,12 +1,14 @@
 import {Router} from "express"
-import {recoverPassword,getPagesRecoverPasswor,getPageSendEmail} from '../controllers/recoverPassword.controller.js'
+import {recoverPassword,getPagesRecoverPasswor,getPageSendEmail,manegesSendEmail} from '../controllers/recoverPassword.controller.js'
 
 const router = Router()
 
-router.get('/recover/password/:id',getPagesRecoverPasswor)
+router.get('/recover/password/:token/:id',getPagesRecoverPasswor)
 
 router.get('/recover/password/',getPageSendEmail)
 
 router.post('/recover/password',recoverPassword)
+
+router.post('/recover/sendEmail',manegesSendEmail)
 
 export default router
