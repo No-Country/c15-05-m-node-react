803d8787dda6d1355885ee08f6d36653370ffb92
Franco <francodeanquin@gmail.com>
2023-12-08 18:30:51 -0300

    implementadas las rutas protegidas

--- front/src/router/Router.jsx
+++ front/src/router/Router.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Landing from "../components/pages/landing/Landing.jsx";
 import RegisterCompany from "../components/register/RegisterCompany.jsx";
 
@@ -10,34 +10,53 @@ import LandingUA from "../components/pagesUA/LandingUA";
 import Catalogo from "../components/pagesUA/Catalogo";
 import Analitics from "../components/pagesUA/Analiticas";
 
-import DetailsProduct from "../components/pagesUA/DetailsProduct"
+import DetailsProduct from "../components/pagesUA/DetailsProduct";
 
 import DashboardGlobal from "../components/dashboard/global/DashboardPage";
 import Login from "../components/register/Login";
 import RegisterUser from "../components/register/RegisterUser";
+import ProtectedRoute from "../components/Utils/ProtectedRoute.jsx";
+import { useLocalStorage } from "react-use";
 
 const router = () => {
+  const [user, setUser] = useLocalStorage("user");
   return (
     <>
-    <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register-company" element={<RegisterCompany />} />
         <Route path="/register-user" element={<RegisterUser />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/ua/landing" element={<LandingUA />}/>{/*"/dashboard/ua/:id" */}
-        <Route path="/ua/product/create" element={<CreateProducts />}/>
-        <Route path="/ua/product/edit" element={<EditProducts />}/>
-        <Route path="/ua/product/inventory" element={<Inventory />}/>
-        <Route path="/ua/product/catalogo" element={<Catalogo />}/>
-        <Route path='/ua/product/detail/:detailId' element={<DetailsProduct />}/>
-        <Route path="/ua/product/analitics" element={<Analitics />}/>
-        <Route path="/Dashboard" element={<DashboardGlobal/>}/>
-    </Routes>
-    </Router>
-  </>
-  
-    
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/landing" element={<LandingUA />} />
+          {/*"/dashboard/ua/:id" */}
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/product/create" element={<CreateProducts />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/product/edit" element={<EditProducts />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/product/inventory" element={<Inventory />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/product/catalogo" element={<Catalogo />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route
+            path="/ua/product/detail/:detailId"
+            element={<DetailsProduct />}
+          />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/ua/product/analitics" element={<Analitics />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={user} />}>
+          <Route path="/Dashboard" element={<DashboardGlobal />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
