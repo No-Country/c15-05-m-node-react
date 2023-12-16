57547e3e029692f24a2b69942a94a7d0ddd4aab0
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-12-16 16:29:17 -0300

    quito componente view en catalogo e inventario

--- front/src/components/pagesUA/Catalogo.jsx
+++ front/src/components/pagesUA/Catalogo.jsx
@@ -1,11 +1,39 @@
+import React, {useEffect, useState} from "react";
 import DashboardPage from "../dashboard/global/DashboardPage";
-import ViewCatalogo from "../Catalogo/viewCatalogo/ViewCatalogo"
+import ButtonBack from "../Utils/Buttons/ButtonBack";
+import HeaderCatalogo from "../Catalogo/header/HeaderCatalogo";
+import CardsContainer from "../Catalogo/cardsContainer/CardsContainer";
+import {useSelector, useDispatch} from "react-redux";
+import { getAllProductsAction } from "../../redux/actionsProducts";
+import { getCompanyAction } from "../../redux/actionsCompany";
 
-const Catalogo = () => {
-    return ( 
-    <DashboardPage>
-        <ViewCatalogo/>
-    </DashboardPage> );
+function Catalogo() {
+    const dispatch = useDispatch();
+    const { user } = useSelector(state => state.user);
+    //const user = useSelector(state => state.user.user)
+     //console.log("USER ", user);
+    //  const { products} = useSelector(state => state.products);
+    const companyId = user.companyID;
+    
+    const [searchQuery, setSearchQuery] = useState("");
+  //console.log("PRDOUCTS GET ALL en view CATALOGO", products);
+      useEffect(() => {
+          dispatch(getAllProductsAction(companyId));
+          dispatch(getCompanyAction(companyId))
+      }, [dispatch, companyId]);
+
+    return (
+        <DashboardPage>
+            <div>
+                <div className="mt-10 ml-10">
+                    <ButtonBack />
+                </div>
+                <HeaderCatalogo setSearchQuery={setSearchQuery} />
+                <CardsContainer searchQuery={searchQuery} />
+            </div>
+        </DashboardPage>
+    );
 }
  
 export default Catalogo;
+
--- front/src/components/pagesUA/Inventory.jsx
+++ front/src/components/pagesUA/Inventory.jsx
@@ -1,10 +1,29 @@
+import React, {useEffect, useState} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllProductsAction } from "../../redux/actionsProducts";
+import { getCompanyAction } from "../../redux/actionsCompany";
 import DashboardPage from "../dashboard/global/DashboardPage";
-import ViewInventario from "../Inventario/ViewInventario";
+import InventarioHeader from "../Inventario/InventarioHeader";
+import InventarioList from "../Inventario/InventarioList";
 
 const Inventory = () => {
+    const dispatch = useDispatch();
+    const { user } = useSelector(state => state.user);
+    //const user = useSelector(state => state.user.user)
+    const companyId = user.companyID;
+    
+    const [searchQuery, setSearchQuery] = useState("");
+    
+      useEffect(() => {
+          dispatch(getAllProductsAction(companyId));
+          dispatch(getCompanyAction(companyId))
+      }, [dispatch, companyId]);
+
+
     return (
     <DashboardPage>
-        <ViewInventario/>
+        <InventarioHeader setSearchQuery={setSearchQuery}/>
+        <InventarioList searchQuery={searchQuery}/>
     </DashboardPage> );
 }
  
